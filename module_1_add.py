eedf46d0378adc18a450a35aa6b375563eb8e0fa
Vladimir <sidorovvp2018@gmail.com>
2024-11-06 16:13:20 +0300

    Учебный проект работа

---
+++ module_1_add.py
@@ -0,0 +1,14 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+students_sorted = sorted(students)
+
+#print(int(sum(grades[0:])))
+#a = dict(zip(students_sorted, grades))
+grades = sum(grades[0][0:5])
+print(grades)
+#grades_average = sum(a[0])
+#print (type(a))
+#print(a)
+#print(type(grades_average))
+#print(grades_average)
+#print(students_sorted)
