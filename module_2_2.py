b7f57f54b0d98fcf4089d5fe13d486391e41adce
Vladimir-SVP <sidorovvp2018@gmail.com>
2024-11-10 17:02:02 +0000

    123

---
+++ module_2_2.py
@@ -0,0 +1,19 @@
+first = input('Введите первое целое число: ')
+if first.isdigit():    
+    second = input('Введите второе целое число: ')
+    if second.isdigit:
+        third = input('Введите третье целое число: ')
+else:
+    print('Вы ввели не целое число!!! Попробуйте заново!')
+#if second == int(second):
+   # third = input('Введите третье целое число: ')
+
+        
+        
+     #   if third == int(third):
+            
+#else:
+  #  print('Вы ввели не целое число!')
+
+#if first == second and first == third:
+    #print('Количество одинаковых чисел:', '3')
