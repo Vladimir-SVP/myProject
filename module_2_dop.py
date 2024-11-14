e8e14cd2278df91285bb890d47043c8d92b9192e
Vladimir-SVP <sidorovvp2018@gmail.com>
2024-11-14 12:39:49 +0000

    done

--- module_2_dop.py
+++ module_2_dop.py
@@ -1,14 +1,18 @@
 def input_number():
-    n = int(input('Введите число от 3 до 20: '))
+    n = 0
     while True:
+        n = int(input('Введите число от 3 до 20: '))
         if n < 3 or n > 20:
             print('Вы ввели не правильное число! Попробуйте ещё раз.')
             continue
         else:
-            break 
+            break
     return(n)
-a = input_number()
-
-print(a)
-# n = list(range(3,21))
-# print(n)
+entered_number = input_number()
+result = []
+for i in range(1,entered_number):
+    for j in range(i+1,entered_number):
+        if entered_number % (i+j) == 0:
+            result.append(i)
+            result.append(j)
+print(f'Пароль для Вашего числа {entered_number}: ', *result, sep='')
