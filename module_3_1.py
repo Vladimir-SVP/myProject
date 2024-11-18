fb5db086b02a270e3ae06cb5e6890718e647af8c
Vladimir-SVP <sidorovvp2018@gmail.com>
2024-11-18 13:49:03 +0000

    done

--- module_3_1.py
+++ module_3_1.py
@@ -7,6 +7,14 @@ def string_info(string):
     result = (len(string), string.upper(), string.lower())
     return(result)
 def is_contains(string, list_to_search):
-    
-print(string_info('Capybara'))
+    count_calls()
+    if string.lower() in list(map(str.lower, list_to_search)):
+        return True
+    else:
+        return False
+print(string_info('MyProject'))
+print(string_info('Instruсtion'))
+print(string_info('Headphones'))
+print(is_contains('Fish', ['fiSH', 'Fishing', 'ing']))
+print(is_contains('mouse', ['muse', 'MOUses']))
 print(calls)
