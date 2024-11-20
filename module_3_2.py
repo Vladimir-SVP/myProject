04612ed79454a33a73156827a0aac286af16a98f
Vladimir-SVP <sidorovvp2018@gmail.com>
2024-11-20 13:49:18 +0000

    done

--- module_3_2.py
+++ module_3_2.py
@@ -1,9 +1,11 @@
 def send_email(message, recipient, sender = "university.help@gmail.com"):
-    domen = ['.com', '.ru', '.net']
-    for i in range(0, len(domen)):
-        if ('@' not in recipient or '@' not in sender) and (domen[i] not in sender[-4:] or domen[i] not in recipient[-4:]):
-            print(sender[-4:])            
-        else:
-            print('2')
-            print(type(domen[i]))
+    domen = ('.com', '.ru', '.net')
+    if '@' not in recipient or '@' not in sender or not recipient.endswith(domen) or not sender.endswith(domen):
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}.')
+    elif sender == recipient:
+        print('Нельзя отправить письмо самому себе.')
+    elif sender == 'university.help@gmail.com':
+        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}.')
+    else:
+        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.')
 send_email('Privet', 'acbf@mail.net')
