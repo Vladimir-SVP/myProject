bee3d6a4c8327a9c93fe91f93ec13663ebf63186
Vladimir-SVP <sidorovvp2018@gmail.com>
2024-11-20 09:52:30 +0000

    123

--- module_3_2.py
+++ module_3_2.py
@@ -1,7 +1,9 @@
 def send_email(message, recipient, sender = "university.help@gmail.com"):
     domen = ['.com', '.ru', '.net']
-    if '@' not in recipient or '@' not in sender and sender[-4:] not in domen[]:
-        print('3')
-    else:
-        print('2')
-send_email('Privet', 'acbfmail.net')
+    for i in range(0, len(domen)):
+        if ('@' not in recipient or '@' not in sender) and (domen[i] not in sender[-4:] or domen[i] not in recipient[-4:]):
+            print(sender[-4:])            
+        else:
+            print('2')
+            print(type(domen[i]))
+send_email('Privet', 'acbf@mail.net')
