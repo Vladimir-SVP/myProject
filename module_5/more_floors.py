cd6ce982d82036bf9499dd1fc2a06571903512c0
Vladimir-SVP <sidorovvp2018@gmail.com>
2025-01-10 12:42:56 +0000

    123

--- module_5/more_floors.py
+++ module_5/more_floors.py
@@ -1,7 +1,7 @@
 class House:
     def __init__(self, name, number_of_floors):
         self.name = name
-        self.number_of_floors = number_of_floors
+        self.number_of_floors = number_of_floors        
     def __len__(self):
         return self.number_of_floors
     def __str__(self):
