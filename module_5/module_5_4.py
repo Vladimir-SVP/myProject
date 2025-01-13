a647a43ed6393c01f51f139480fc5bcd4686097f
Vladimir-SVP <sidorovvp2018@gmail.com>
2025-01-13 08:30:19 +0000

    done

--- module_5/module_5_4.py
+++ module_5/module_5_4.py
@@ -1,17 +1,17 @@
 class House:
     houses_history = []
     def __new__(cls, *args, **kwargs):
-         return cls.houses_history
+        cls.houses_history.append(args[0])
+        return super().__new__(cls)
     def __init__(self, name, number_of_floors):
         self.name = name
-        self.number_of_floors = number_of_floors        
+        self.number_of_floors = number_of_floors       
     def __len__(self):
         return self.number_of_floors
     def __str__(self):
         return f'Название: {self.name}, количество этажей: {self.number_of_floors}'
-    def __del__(self):
-        return f'{name} снесён, но он останется в истории'
-    
+    def __del__(self):        
+        print(self.name)    
 h1 = House('ЖК Эльбрус', 10)
 print(House.houses_history)
 h2 = House('ЖК Акация', 20)
@@ -22,3 +22,15 @@ print(House.houses_history)
 del h2
 del h3
 print(House.houses_history)
+
+# class Example():
+#     def __new__(cls, *args, **kwargs):
+#         print(args)
+#         print(kwargs)
+#         return super().__new__(cls)
+#     def __init__(self, first, second, third):
+#         print(first)
+#         print(second)
+#         print(third)
+# ex = Example('data', second=25, third=3.14)
+
