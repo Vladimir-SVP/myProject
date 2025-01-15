d3a7ee75bb5f4509d95c15b8615c60f756e5c4d0
Vladimir-SVP <sidorovvp2018@gmail.com>
2025-01-15 10:27:20 +0000

    done

--- module_5/urtube.py
+++ module_5/urtube.py
@@ -1,15 +1,15 @@
-class User():
+class User:
     def __init__(self, nickname, password, age):
         self.nickname = nickname
         self.password = password
         self.age = age
-class Video():
+class Video:
     def __init__(self, title, duration, time_now = 0, adult_mode = False):
         self.title = title
         self.duration = duration
         self.time_now = time_now
         self.adult_mode = adult_mode        
-class UrTube():
+class UrTube:
     videos = []
     def __add__(self, ):
         
