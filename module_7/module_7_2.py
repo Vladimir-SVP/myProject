fa488a5da596438d6b7cbcb70ea223f6820d86eb
Vladimir <sidorovvp2018@gmail.com>
2025-01-23 11:05:31 +0300

    done

--- module_7/module_7_2.py
+++ module_7/module_7_2.py
@@ -1,18 +1,21 @@
 def custom_write(file_name, strings):
-    file = open(file_name, 'a+', encoding = 'utf-8')
-    for i in strings:
-        file.write((f'{i}\n'))
-        line = file.readline()
-        print(line)
+    file = open(file_name, 'w+', encoding = 'utf-8')
+    strings_positions = {}
+    for i in range(len(strings)):
+        j = file.tell()
+        file.write((f'{strings[i]}\n'))
+        strings_positions.update({(i+1, j) : strings[i]})
     file.close
-    return 
-
+    return strings_positions
+  
 info = [
     'Text for tell.',
     'Используйте кодировку utf-8.',
     'Because there are 2 languages!',
     'Спасибо!'
     ]
+print('=======================')
 result = custom_write('test.txt', info)
 for elem in result.items():
     print(elem)
+print()
